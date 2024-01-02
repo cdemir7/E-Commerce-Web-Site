3b382fc35b03d48761aa3f0f07149f46f70fa693
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2024-01-02 17:21:21 +0300

    This is a new commit for what I originally planned to be amended

---
+++ src/main/java/com/example/ecommercewebsite/ECommerceWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceWebSiteApplication.class, args);
+    }
+
+}
